4db4be3342ae5686ffbe6cf75f5b0725cda049cc
Matthew Beech <matthew_beech@hotmail.com>
2021-02-17 20:01:45 +0000

    update gulpfile.js file to work with Tailwind CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 const { src, dest, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-
-sass.compiler = require('sass');
+const postcss = require('gulp-postcss');
 
 function css(cb) {
-  src('./src/sass/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
+  src('./src/css/*.css')
+    .pipe(postcss([require('tailwindcss'), require('autoprefixer')]))
     .pipe(dest('./public/css/'));
   cb();
 }
 
 function watcher(cb) {
-  watch('./src/sass/*.scss').on('change', series(css, browserSync.reload));
+  watch('./src/css/*.css').on('change', series(css, browserSync.reload));
+  watch(['./tailwind.config.js', './postcss.config.js', './gulpfile.js'], css).on('change', browserSync.reload);
   cb();
 }
 
