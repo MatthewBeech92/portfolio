f543861febdee1519635dfdaceb7d6135adb2907
Matthew Beech <matthew_beech@hotmail.com>
2021-02-19 10:10:44 +0000

    Add TailwindCSS Config to Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,44 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: false,
+    content: ['./public/index.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      white: '#fff',
+      grey: colors.trueGray,
+      teal: colors.teal,
+      blue: colors.lightBlue,
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif'],
+    },
+    zIndex: {
+      '-50': '-50',
+      '-40': '-40',
+      '-30': '-30',
+      '-20': '-20',
+      '-10': '-10',
+      0: 0,
+      10: 10,
+      20: 20,
+      30: 30,
+      40: 40,
+      50: 50,
+      auto: 'auto',
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        'header-mobile': "url('/images/header-background-mobile.jpg')",
+        'header-desktop': "url('/images/header-background-desktop.jpg')",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
