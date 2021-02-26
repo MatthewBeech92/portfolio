5cfe615a3f4cd126c58b0d7811c4aa0f1f0fb2fe
Matthew Beech <matthew_beech@hotmail.com>
2021-02-26 00:48:02 +0000

    Add ajax function to use with contact form

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,32 @@ $('#home a').on('click', function (evt) {
     );
   }
 });
+
+// Contact Form
+
+$('#contact-form').on('submit', function (evt) {
+  evt.preventDefault();
+
+  const data = $(this).serialize();
+
+  $.ajax({
+    url: '../contact.php',
+    data: data,
+    method: 'POST',
+    error: function (jqXHR) {
+      const errors = JSON.parse(jqXHR.responseText);
+
+      $('.alert').remove();
+
+      $.each(errors, function (index, value) {
+        $('#' + index).after('<div class="alert error">' + value + '</div>');
+      });
+    },
+    success: function (successMsg) {
+      $('.alert').remove();
+
+      $('#contact-form').prepend('<div class="alert success">' + successMsg + '</div>');
+      $('#contact-form').trigger('reset');
+    },
+  });
+});
